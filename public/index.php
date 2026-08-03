8d5be9d34c2a07e579c1e3b5454492f2d33908ee
Christopher Oduor <149111523+Gggghgggh@users.noreply.github.com>
2026-08-03 13:18:51 +0300

    Configure public index for shared hosting

--- public/index.php
+++ public/index.php
@@ -5,16 +5,18 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$projectRoot = '/home/mlyycypz/Scented-Muse-Shop';
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $projectRoot.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $projectRoot.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $projectRoot.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
